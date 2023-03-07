1b2c59e884c75b75e19dc99b3a6e5286923a4bf7
Rafal Burian <rafalburian@gmail.com>
2023-03-07 18:24:54 +0100

    updates

--- client/src/app/features/appSlice.ts
+++ client/src/app/features/appSlice.ts
@@ -4,8 +4,8 @@ import { AppState } from "../../types/appState"
 
 const initialState: AppState = {
 	token: null,
-	serverUrl: import.meta.env.VITE_SERVER_URL || "http://localhost:4000",
-	appUrl: import.meta.env.VITE_CLIENT_URL || "http://localhost:5173",
+	serverUrl: import.meta.env.VITE_SERVER_URL || "http://localhost:4000",//
+	appUrl: import.meta.env.VITE_CLIENT_URL || "http://localhost:5173",//
 	alerts: [],
 	alertLifeTime: 5000,
 	isAppLoading: false,
